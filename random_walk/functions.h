df361a7be3028f701c3b9df0607641724eae0884
Matthias Maile <matthias.maile1@gmail.com>
2020-07-20 16:37:41 +0200

    header file for functions

---
+++ random_walk/functions.h
@@ -0,0 +1,14 @@
+/*** functions.h start ***/
+
+#include <tuple>
+
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+std::tuple<int, int> random_walk(unsigned int n);
+
+double random_walk_distance(unsigned int n);
+
+#endif
+
+/*** functions.h end ***/
