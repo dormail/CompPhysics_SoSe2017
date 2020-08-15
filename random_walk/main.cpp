eb5413ed67735a6abe12431bf46a393fb5e06afd
Matthias Maile <matthias.maile1@gmail.com>
2020-08-15 20:10:03 +0200

    runtime values changed

--- random_walk/main.cpp
+++ random_walk/main.cpp
@@ -18,7 +18,7 @@ int main(int argc, char** argv){
 
 	// n reichweite des walks
 	// N anzahl an tests
-	unsigned int n = 100;
+	unsigned int n = 150;
 	unsigned int N = 10000;
 
 	// liest input argument
