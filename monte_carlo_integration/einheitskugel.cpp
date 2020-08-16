27fd260b2bb98f14830694a02d94dbf7fa394d6e
Matthias Maile <matthias.maile1@gmail.com>
2020-08-16 10:02:45 +0200

    cleaned up code

--- monte_carlo_integration/einheitskugel.cpp
+++ monte_carlo_integration/einheitskugel.cpp
@@ -3,16 +3,17 @@
 #include <iostream>
 #include <time.h>
 #include <random>
-#include <cmath> // mathematische funktionen
+#include <cmath>
 
 
 // function returning the distance from origin 
-double distance(double x, double y, double z){
+double distance(double x, double y, double z)
+{
 	return x*x + y*y + z*z;
-	//return sqrt(x*x + y*y + z*z); da radiu = 1 ist egal ob wurzel oder nicht
 }
 
-int main(){
+int main()
+{
 	// random number generator
 	std::random_device rd;
 	std::mt19937 gen(rd());
@@ -27,7 +28,8 @@ int main(){
 	// counter for how many have been inside the sphere
 	unsigned int n = 0;
 
-	for (int i = 0; i < N; ++i){
+	for (int i = 0; i < N; ++i)
+	{
 		// setting new coordinates and distance
 		x = dis(gen);
 		y = dis(gen);
@@ -36,14 +38,13 @@ int main(){
 
 		// checking if point is inside the sphere
 		if (r <= 1) ++n;
-
 		//std::cout << r << '\t';
 		//if (i % 3 == 0) std::cout << '\n';
 	}
 
-	// calculating the probability of a evalutation laying inside the sphere
-	// based on V_cube = 2^3 = 8 and p calculating the Volume of the sphere, 
-	// based on V_sphere calculating pi
+	/* calculating the probability of a evalutation laying inside the sphere
+	 * based on V_cube = 2^3 = 8 and p calculating the Volume of the sphere, 
+	 * based on V_sphere calculating pi */
 
 	double p = n /(double)N;
 	double V_sphere = p * 8;
