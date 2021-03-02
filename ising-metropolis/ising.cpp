03b4d610ac70d4dd5665375576db89f6167bebf9
Matthias Maile <matthias.maile@udo.edu>
2021-03-02 15:17:56 +0100

    fixed bad contruction for rng

--- ising-metropolis/ising.cpp
+++ ising-metropolis/ising.cpp
@@ -17,11 +17,12 @@ ising::ising(size_t n) : grid(n*n), N(n)
 ising::ising(size_t n, double b) :
 grid(n*n),
 N(n),
-beta(b)
+beta(b),
+dis_int(0, n-1)
 {
     std::mt19937 gen(rd());
     std::uniform_real_distribution<> dis_real(0.0, 1.0);
-    std::uniform_int_distribution<> dis_int(0,n-1);
+    //std::uniform_int_distribution<> dis_int(0,n-1);
 }
 
 int ising::get_spin(int x, int y) {
