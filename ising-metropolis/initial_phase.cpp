972d144b549cea7266b8bd1450ec20ea656477ad
Matthias Maile <matthias.maile@udo.edu>
2021-03-03 09:29:32 +0100

    extra main to compare initial phase

---
+++ ising-metropolis/initial_phase.cpp
@@ -0,0 +1,28 @@
+//
+// Created by mmaile on 3/2/21.
+//
+
+#include "ising.h"
+#include <string>
+#include <chrono>
+
+int main(int argc, char *argv[]){
+    double beta = 1.;
+    if(argc >= 2){
+        beta = atof(argv[1]);
+    }
+    unsigned int seed = std::chrono::system_clock::now().time_since_epoch().count();
+    ising grid(100, beta, seed);
+    grid.set_all(1);
+
+    std::string argv2;
+    if(argc >= 3){
+        argv2.assign(argv[2]);
+        if(argv2.compare("--random") == 0)
+            grid.random();
+    }
+
+    grid.simulate_initial_phase();
+
+    return 0;
+}
