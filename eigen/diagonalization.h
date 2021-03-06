61ccee0bf3bef4b29eeefabf7fbd72525d61ad6e
Matthias Maile <matthias.maile@udo.edu>
2021-03-06 15:56:56 +0100

    helper function calculating the error

--- eigen/diagonalization.h
+++ eigen/diagonalization.h
@@ -7,8 +7,11 @@
 
 #include <Eigen/Dense>
 
-Eigen::MatrixXd jacobi_rotation(Eigen::MatrixXd A, double eps);
 Eigen::MatrixXd householder(Eigen::MatrixXd A);
+
+double off(Eigen::MatrixXd A);
+Eigen::MatrixXd jacobi_rotation(Eigen::MatrixXd A);
+Eigen::MatrixXd jacobi_rotation(Eigen::MatrixXd A, double eps);
 
 // helper functions
 double sgn(double x);
--- eigen/jacobi_rotation.cpp
+++ eigen/jacobi_rotation.cpp
@@ -4,6 +4,23 @@
 
 #include "diagonalization.h"
 #include <math.h>
+#include <iostream>
+
+double off(Eigen::MatrixXd A)
+{
+    int n = A.rows();
+    double sum = 0;
+    for (unsigned int i = 0; i < n; ++i)
+    {
+        for (unsigned int j = 0; j < n; ++j)
+        {
+            if (i == j)
+                continue;
+            sum += pow(A(i, j), 2);
+        }
+    }
+    return sum;
+}
 
 Eigen::MatrixXd jacobi_rotation(Eigen::MatrixXd A, double eps)
 {
