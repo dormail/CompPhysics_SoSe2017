2a5a4019f9ac9157562f6806aa502e8c47ef67a3
Matthias Maile <matthias.maile@udo.edu>
2021-03-11 19:35:20 +0100

    function to plot from txt

--- harmonic_oscillator/plot.py
+++ harmonic_oscillator/plot.py
@@ -2,15 +2,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-fn = 'res.txt'
-data = np.genfromtxt(fn)
+def PlotFromTxt(input, output):
+    data = np.genfromtxt(input)
 
-x = data[0::3]
-y = data[1::3]
-z = data[2::3]
+    x = data[0::3]
+    y = data[1::3]
+    z = data[2::3]
 
-plt.plot(x, label='x(t)')
-plt.plot(y, label='y(t)')
-plt.plot(z, label='z(t)')
-plt.legend()
-plt.savefig('plot.pdf')
+    plt.plot(x, label='x(t)')
+    plt.plot(y, label='y(t)')
+    plt.plot(z, label='z(t)')
+    plt.legend()
+    plt.savefig(output)
+    plt.clf()
+
+
+# plotting the euler stuff
+PlotFromTxt('res.txt', 'plot.pdf')
